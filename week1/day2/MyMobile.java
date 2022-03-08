823c621879f0d13c223ee06d49e38b2af026bda8
Ramakrishna-1989 <71293725+Ramakrishna-1989@users.noreply.github.com>
2022-03-08 22:04:43 +0530

    Initial commit

---
+++ week1/day2/MyMobile.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+import week1.day1.Mobile;
+
+public class MyMobile {
+
+public static void main(String args[]) {
+		
+		Mobile obj = new Mobile();
+		obj.makeCall();
+		obj.sendMsg();
+		
+	}
+}
